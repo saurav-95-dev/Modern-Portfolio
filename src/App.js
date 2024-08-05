23f2836a0794519d0169a3367c179027697a22de
Saurabh <devsaurabh086@gmail.com>
2024-08-05 17:31:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+import { darkTheme, lightTheme } from './utils/Themes.js';
+import Navbar from "./components/Navbar";
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import ProjectDetails from "./components/ProjectDetails";
+import styled from "styled-components";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`;
+
+const ToggleButton = styled.button`
+  position: fixed;
+  top: 23px;
+  right: 285px;
+  background: ${({ theme }) => theme.toggleBg};
+  color: ${({ theme }) => theme.toggleColor};
+  border: none;
+  padding: 5px 5px;
+  padding-top:8px;
+  padding-bottom: 8px;
+  padding-left:8px;
+  padding-right:8px;
+  cursor: pointer;
+  border-radius: 8px;
+  z-index: 1000;
+`;
+
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+
+  const toggleTheme = () => {
+    setDarkMode(prevMode => !prevMode);
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <Router>
+          <Navbar />
+          <ToggleButton onClick={toggleTheme}>
+            {darkMode ? 'Light' : 'Dark'}
+          </ToggleButton>
+          <Body>
+            <HeroSection />
+            <Wrapper>
+              <Skills />
+              <Experience />
+            </Wrapper>
+            <Projects openModal={openModal} setOpenModal={setOpenModal} />
+            <Wrapper>
+              <Education />
+              <Contact />
+            </Wrapper>
+            <Footer />
+            {openModal.state &&
+              <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+            }
+          </Body>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Context.js
@@ -0,0 +1,3 @@
+import React from "react";
+const context = React.createContext('Saurabh dev');
+export default context;
